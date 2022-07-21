00c31da86d725820952780cd750e2f80fc3d9206
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-07-21 13:07:24 +0000

    implemented content negotiation via headers

--- src/main/java/com/ratz/restfullapi/config/WebConfig.java
+++ src/main/java/com/ratz/restfullapi/config/WebConfig.java
@@ -11,9 +11,20 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
-    configurer.favorParameter(true)
-        .parameterName("mediaType")
-        .ignoreAcceptHeader(true)
+    //content negotiation via query param
+//    configurer.favorParameter(true)
+//        .parameterName("mediaType")
+//        .ignoreAcceptHeader(true)
+//        .useRegisteredExtensionsOnly(false)
+//        .defaultContentType(MediaType.APPLICATION_JSON)
+//        .mediaType("json", MediaType.APPLICATION_JSON)
+//        .mediaType("xml", MediaType.APPLICATION_XML);
+//  }
+
+
+    //content negotiation via headers
+    configurer.favorParameter(false)
+        .ignoreAcceptHeader(false)
         .useRegisteredExtensionsOnly(false)
         .defaultContentType(MediaType.APPLICATION_JSON)
         .mediaType("json", MediaType.APPLICATION_JSON)
