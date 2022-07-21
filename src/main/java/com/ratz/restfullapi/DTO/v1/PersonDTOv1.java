a6a48c4770aa1ac9494b8eb41212532e027166d2
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-07-21 13:04:44 +0000

    added content negotiation via query param

--- src/main/java/com/ratz/restfullapi/DTO/v1/PersonDTOv1.java
+++ src/main/java/com/ratz/restfullapi/DTO/v1/PersonDTOv1.java
@@ -1,18 +1,28 @@
 package com.ratz.restfullapi.DTO.v1;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serial;
 import java.io.Serializable;
 
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonDTOv1 implements Serializable {
 
   @Serial
   private static final long serialVersionUID = -5452647583822206968L;
 
-
   private Long id;
+
+  @JsonProperty("first_name")
   private String firstName;
+
+  @JsonProperty("last_name")
   private String lastName;
+
   private String address;
+
+  //@JsonIgnore
   private String gender;
 
   public PersonDTOv1() {
--- src/main/java/com/ratz/restfullapi/controller/PersonController.java
+++ src/main/java/com/ratz/restfullapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.ratz.restfullapi.DTO.v1.PersonDTOv1;
 import com.ratz.restfullapi.DTO.v2.PersonDTOv2;
 import com.ratz.restfullapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,38 +18,44 @@ public class PersonController {
   @Autowired
   private PersonService personService;
 
-  @GetMapping("/{id}")
+  @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
   public PersonDTOv1 findById(@PathVariable(value = "id") String id) {
 
     return personService.findById(id);
   }
 
-  @GetMapping
+  @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
   public List<PersonDTOv1> findAll() {
 
     return personService.findAll();
   }
 
-  @PostMapping
+  @PostMapping(
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+      consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
   public PersonDTOv1 createPerson(@RequestBody PersonDTOv1 person) {
 
     return personService.createPerson(person);
   }
 
-  @PostMapping("/api/person/v2")
+  @PostMapping(value = "/api/person/v2",
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+      consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
   public PersonDTOv2 createPersonV2(@RequestBody PersonDTOv2 person) {
 
     return personService.createPersonV2(person);
   }
 
-  @DeleteMapping("/{id}")
+  @DeleteMapping(value = "/{id}")
   public ResponseEntity<?> deletePerson(@PathVariable String id) {
 
     personService.deletePerson(id);
     return ResponseEntity.noContent().build();
   }
 
-  @PutMapping
+  @PutMapping(
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+      consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
   public PersonDTOv1 updatePerson(@RequestBody PersonDTOv1 person) {
 
     return personService.updatePerson(person);
