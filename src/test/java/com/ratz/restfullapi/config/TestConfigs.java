8071e6820b55e0463df2d4bb4274d9288528f470
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-08-03 14:58:22 +0000

    created docker file, docker compose, docker image, published image in docker hub and ran the application in the docker

--- src/test/java/com/ratz/restfullapi/config/TestConfigs.java
+++ src/test/java/com/ratz/restfullapi/config/TestConfigs.java
@@ -2,7 +2,7 @@ package com.ratz.restfullapi.config;
 
 public class TestConfigs {
 
-  public static final int SERVER_PORT = 8888;
+  public static final int SERVER_PORT = 80;
   public static final String HEADER_PARAM_ORIGIN = "Origin";
   public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
   public static final String CONTENT_TYPE_JSON = "application/json";
